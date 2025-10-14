dd8662f221f9ffe2abcf749932c17ada5fc4b43f
Flavia Kosti <flaviakosti@gmail.com>
2025-10-13 20:55:46 -0400

    Css Playground: Layout P2

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app-routing';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    // provideRouter(routes)
   ]
 }).catch(err => console.error(err));
