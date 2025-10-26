dd4f70f092d6eaa3396dfd53774361d65a922004
Flavia Kosti <flaviakosti@gmail.com>
2025-10-26 12:32:29 -0400

    Css Playground: Preview P1

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,10 +1,14 @@
+import { KeyValuePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ELEMENTS } from 'src/app/app.config';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  styleUrls: ['./preview.component.scss'],
+  imports: [KeyValuePipe, MatButtonModule, MatMenuModule]
 })
 export class PreviewComponent {
   readonly elements = ELEMENTS;
