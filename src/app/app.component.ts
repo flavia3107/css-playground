428a367030f8412d39cfa46080a60e19166a9d78
Flavia Kosti <flaviakosti@gmail.com>
2025-10-29 19:41:22 -0400

    Css Playground: Export P3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { CssCodeComponent } from './components/css-code/css-code.component';
 import { CssConfigsComponent } from './components/css-configs/css-configs.component';
@@ -9,7 +11,7 @@ import { CssConfigService } from './services/cssconfig.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, CssConfigsComponent, PreviewComponent, CssCodeComponent]
+  imports: [RouterModule, CssConfigsComponent, PreviewComponent, CssCodeComponent, MatButtonModule, MatIconModule]
 })
 export class AppComponent {
   private _cssConfigService = inject(CssConfigService);
