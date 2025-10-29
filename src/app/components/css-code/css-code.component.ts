e7c6e4ae3889e9f1a01bd02dd5fc0eaab78976e7
Flavia Kosti <flaviakosti@gmail.com>
2025-10-29 19:27:33 -0400

    Css Playground: Export P1

---
+++ src/app/components/css-code/css-code.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css-code',
+  templateUrl: './css-code.component.html',
+  styleUrls: ['./css-code.component.scss']
+})
+export class CssCodeComponent {
+
+}
