ed59148e45ba261d026a4b42d2e6a0fab0c5c60b
Flavia Kosti <flaviakosti@gmail.com>
2025-11-02 15:59:00 -0500

    Css Playground: Theme P4

--- src/shared/configs/theme.model.ts
+++ src/shared/configs/theme.model.ts
@@ -4,18 +4,28 @@ export interface Theme {
 }
 
 export const defaultTheme: Theme = {
-	name: "defaultTheme",
+	name: 'defaultTheme',
 	properties: {
-		"--bg-color": "#f7f6f7",
-		"--gray": "#d3d3d3"
+		'--bg-color': '#f7f6f7',
+		'--gray': '#d3d3d3',
+		'--card-bg-color': '#fefefe',
+		'--gray-light-color': '#f0f0f0',
+		'--secondary-bg-color': '#f8f8f8',
+		'--light-gray-border': '#c0c0c0'
 	}
 };
 
 export const darkTheme: Theme = {
-	name: "darkTheme",
+	name: 'darkTheme',
 	properties: {
-		"--bg-color": "red",
-		"--gray": 'blue'
+		'--bg-color': 'red',
+		'--gray': 'blue',
+		'--card-bg-color': 'yellow',
+		'--gray-light-color': 'purple',
+		'--secondary-bg-color': 'green',
+		'--light-gray-border': 'pink'
+
+
 
 	}
 };
