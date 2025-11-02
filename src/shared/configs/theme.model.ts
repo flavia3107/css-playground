16814dd027fd0db210f2428e6447d7cb6c70e1c8
Flavia Kosti <flaviakosti@gmail.com>
2025-11-02 17:33:13 -0500

    Css Playground: Theme P6

--- src/shared/configs/theme.model.ts
+++ src/shared/configs/theme.model.ts
@@ -6,6 +6,7 @@ export interface Theme {
 export const defaultTheme: Theme = {
 	name: 'defaultTheme',
 	properties: {
+		'--main-text': '#000',
 		'--bg-color': '#f7f6f7',
 		'--gray': '#d3d3d3',
 		'--card-bg-color': '#fefefe',
@@ -19,27 +20,30 @@ export const defaultTheme: Theme = {
 		'--panel-title': '#5e5e5e',
 		'--description-text': '#818181',
 		'--light-divider': '#ccc',
-		'--secondary-text': '#7c7c7c'
+		'--secondary-text': '#7c7c7c',
+		'--input-container-bg': '#f8f8f8'
 	}
 };
 
 export const darkTheme: Theme = {
 	name: 'darkTheme',
 	properties: {
-		'--bg-color': 'red',
-		'--gray': 'blue',
-		'--card-bg-color': 'yellow',
-		'--gray-light-color': 'purple',
-		'--secondary-bg-color': 'green',
-		'--light-gray-border': 'pink',
+		'--main-text': '#fff',
+		'--bg-color': '#222222',
+		'--gray': '#474747',
+		'--card-bg-color': '#292929',
+		'--gray-light-color': '#161616',
+		'--secondary-bg-color': '#f3f3f3',
+		'--light-gray-border': '#dfdfdf',
 		'--mid-gray': 'brown',
 		'--dark-gray': 'coral',
 		'--darker-gray-border': '#000',
 		'--logo-text': '#fff',
-		'--panel-title': 'salmon',
+		'--panel-title': '#dce4ef',
 		'--description-text': 'lightgray',
-		'--light-divider': 'orange',
-		'--secondary-text': 'turquise'
+		'--light-divider': '#585858',
+		'--secondary-text': 'turquise',
+		'--input-container-bg': '#2d2d2d'
 	}
 };
 
