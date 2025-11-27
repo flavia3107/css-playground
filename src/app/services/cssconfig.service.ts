9e251f05c2698d52610e79a02c1dc86568dc2540
Flavia Kosti <flaviakosti@gmail.com>
2025-11-26 21:24:48 -0500

    Css Playground: Add screenshot download P2

--- src/app/services/cssconfig.service.ts
+++ src/app/services/cssconfig.service.ts
@@ -1,5 +1,5 @@
 import { ElementRef, Injectable, signal } from '@angular/core';
-import html2canvas from 'html2canvas';
+import * as htmlToImage from 'html-to-image';
 import { CSS_CONFIG, MULTI_VALUE_MAP, SHORT_HAND_MAP } from '../app.config';
 
 @Injectable({ providedIn: 'root' })
@@ -133,18 +133,12 @@ export class CssConfigService {
 
   downloadScreenshot(captureArea: ElementRef) {
     const element = captureArea.nativeElement;
-
-    html2canvas(element, { useCORS: true }).then(canvas => {
-      canvas.toBlob(blob => {
-        if (blob) {
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement('a');
-          link.href = url;
-          link.download = 'playground-screenshot.png';
-          link.click();
-          URL.revokeObjectURL(url);
-        }
+    htmlToImage.toPng(element)
+      .then(dataUrl => {
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'screenshot.png';
+        link.click();
       });
-    });
   }
 }
